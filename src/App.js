3ef23fbe732f5ac20316b4ef6f2c201e354e7719
Telmo Sampaio <telmoasampaio@gmail.com>
2019-03-11 18:10:37 +0000

    single page application with react

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+import './css/style.css';
+
+import Navbar from './components/navbar';
+import Home from './components/home';
+import ActorsContainer from './components/actorsContainer';
+import Actress from './components/actress';
+import Films from './components/films';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Navbar />
+                    <Route exact path="/" render={() => <Home title="Oscar Winners" />} />
+                    <Route path="/actors/" render={() => <ActorsContainer title="Best Actors" />} />
+                    <Route path="/actress/" render={() => <Actress title="Best Actress" />} />
+                    <Route path="/films" render={() => <Films title="Best Films" />} />
+                </div>
+            </BrowserRouter>
+        );
   }
 }
 
